6814ea10b1ca52edad7ed67fa02f4f6f9f9c2822
AceXiamo <xm17906193@163.com>
2023-12-09 21:04:59 +0800

    Refactor flowStore.ts to include additional properties and update watch function

--- store/flowStore.ts
+++ store/flowStore.ts
@@ -1,14 +1,29 @@
 import { defineStore } from 'pinia'
-import type { GraphNode } from '@vue-flow/core'
+import type { GraphEdge, GraphNode } from '@vue-flow/core'
 
 export const useFlowStore = defineStore('flow', () => {
-  const node = ref<any>({
-    id: '',
-    label: '',
-    style: {
-      background: '',
-    },
-  })
+  const node = ref<any>()
+  const showForm = ref(false)
+  const nodes = ref<GraphNode[]>([])
+  const edges = ref<GraphEdge[]>([])
 
-  return { node }
+  watch(
+    () => node.value,
+    (value) => {
+      if (value)
+        edges.value.forEach((edge) => {
+          if (edge.source === value?.id || edge.target === value?.id) {
+            edge.style = {
+              stroke: '#10b981',
+            }
+            edge.animated = true
+          } else {
+            edge.style = defaultEdgeStyle
+            edge.animated = false
+          }
+        })
+    }
+  )
+
+  return { node, showForm, nodes, edges }
 })
