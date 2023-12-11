88a10d136d16a62f2d058cba0ed7d460836d74a3
AceXiamo <xiamo@qwq.link>
2023-12-11 19:00:48 +0800

    Add NodeButton component

---
+++ components/NodeButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    :class="[
+      `h-[25px] w-[25px] rounded-[5px] grid place-content-center cursor-pointer`,
+      `transition-all duration-200 hover:bg-white/10 active:opacity-60`,
+    ]"
+  >
+    <UIcon :name="$props.icon" class="text-[15px]" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{ icon: string }>()
+</script>
