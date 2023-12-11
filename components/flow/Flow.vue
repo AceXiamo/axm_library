4e8fb9371647079a73d1aed23857187ee75782e2
AceXiamo <xiamo@qwq.link>
2023-12-11 18:59:40 +0800

    Update FlowNode component in Flow.vue

--- components/flow/Flow.vue
+++ components/flow/Flow.vue
@@ -7,7 +7,7 @@
     @nodeClick="nodeClick"
   >
     <template #node-custom="customNodeProps">
-      <FlowNode :data="customNodeProps"></FlowNode>
+      <FlowNode :node="customNodeProps" @addNode="addNode"></FlowNode>
     </template>
   </VueFlow>
   <FlowForm
@@ -39,11 +39,10 @@
 <script lang="ts" setup>
 import { VueFlow, useVueFlow } from '@vue-flow/core'
 import { useFlowStore } from '@/store/flowStore'
-import {
-  resetToDefaultNodeStyle,
-  resetToDefaultEdgeStyle,
-} from '@/utils/flow'
+import { resetToDefaultNodeStyle, resetToDefaultEdgeStyle } from '@/utils/flow'
 import { useMotions } from '@vueuse/motion'
+import { flowData } from './index'
+import type { FlowItem } from './index'
 
 const flowStore = useFlowStore()
 const motions = useMotions()
@@ -51,48 +50,28 @@ const motions = useMotions()
 const nodeClick = (e: any) => {
   const { node } = e
   flowStore.node = node
-  flowStore.showForm = true
 }
 
-const { addEdges, onConnect, onPaneReady, toObject, nodes, edges } =
-  useVueFlow()
+const {
+  addEdges,
+  onConnect,
+  onPaneReady,
+  toObject,
+  nodes,
+  edges,
+  fitView,
+  setState,
+  $reset,
+} = useVueFlow()
 
 onConnect((params) => addEdges(params))
 
-const elements = ref<any>([
-  {
-    id: '1',
-    label: 'node 1',
-    type: 'custom',
-    position: { x: 100, y: 500 },
-  },
-  {
-    id: '2',
-    type: 'custom',
-    label: 'node 2',
-    position: { x: 500, y: 400 },
-  },
-  {
-    id: '3',
-    type: 'custom',
-    label: 'node 3',
-    position: { x: 500, y: 600 },
-  },
-  {
-    id: 'e1-2',
-    target: '2',
-    source: '1',
-  },
-  {
-    id: 'e2-3',
-    target: '3',
-    source: '2',
-  },
-])
+const elements = ref<any>([])
 
 onPaneReady(({ fitView }) => {
   flowStore.nodes = nodes.value
   flowStore.edges = edges.value
+  fitView()
 })
 
 const save = () => {
@@ -100,11 +79,105 @@ const save = () => {
 }
 
 const paneClickHandle = () => {
-  flowStore.node = {}
+  flowStore.node = {
+    data: {
+      bgColor: '#101827',
+    },
+  }
   motions['flow_form']?.leave(() => {
     flowStore.showForm = false
   })
   resetToDefaultNodeStyle(nodes.value)
   resetToDefaultEdgeStyle(edges.value)
 }
+
+const addNode = (data: FlowItem) => {
+  const pNode = nodes.value.find((item) => item.id === data.pid)?.data
+  if (!pNode.children) pNode.children = []
+  pNode.children.push(data)
+  flowNodes.value = []
+  flattenNodes(flowData)
+  console.log(flowNodes.value)
+  elements.value = flowNodes.value
+}
+
+type NodeData = {
+  id: string
+  type?: string
+  position?: {
+    x: number
+    y: number
+  }
+  data?: {
+    label: string
+    desc: string
+    bgColor: string
+  }
+  target?: string
+  source?: string
+}
+
+const NODE_WIDTH = 300,
+  NODE_HEIGHT = 77,
+  GAP = 50
+
+const generateUniqueId = () => Math.random().toString(36).substr(2, 9)
+
+const flowNodes = ref<NodeData[]>([])
+function flattenNodes(
+  data: FlowItem[],
+  level: number = 0,
+  parentNode?: FlowItem
+) {
+  data.forEach((item, index) => {
+    let lastY = 0
+    if (index === 0) lastY += parentNode?.position?.y_min || 0
+    else lastY += (data[index - 1].position?.y_max || 0) + GAP
+
+    item.position = {
+      y_min: lastY,
+      y_max: lastY + NODE_HEIGHT,
+    }
+
+    if (item.children) flattenNodes(item.children, level + 1, item)
+
+    const x = level * (NODE_WIDTH + GAP)
+    const y = calculateYPosition(item, lastY)
+    let node: NodeData = {
+      id: item.id!,
+      type: 'custom',
+      position: { x, y },
+      data: item,
+    }
+    flowNodes.value.push(node)
+    if (parentNode) {
+      parentNode.position!.y_max = Math.max(
+        parentNode.position!.y_max,
+        item.position.y_max
+      )
+      const edge = {
+        id: generateUniqueId(),
+        source: parentNode.id,
+        target: node.id,
+      }
+      flowNodes.value.push(edge)
+    }
+  })
+}
+
+const calculateYPosition = (item: FlowItem, lastY: number) => {
+  if (item.children && item.children.length > 1) {
+    return (
+      item.position!.y_min +
+      (item.position!.y_max - item.position!.y_min - NODE_HEIGHT) / 2
+    )
+  } else {
+    return lastY
+  }
+}
+
+onMounted(() => {
+  flattenNodes(flowData)
+  elements.value = flowNodes.value
+})
 </script>
