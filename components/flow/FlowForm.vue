9c60c7e7df31b2a5459e8b0233d793208513046e
AceXiamo <xiamo@qwq.link>
2023-12-11 19:00:23 +0800

    Update FlowForm.vue with new data bindings

--- components/flow/FlowForm.vue
+++ components/flow/FlowForm.vue
@@ -1,13 +1,16 @@
 <template>
   <div
-    class="absolute m-3 top-0 right-0 bg-black border border-white/10 w-[200px] min-h-[50px] rounded-md p-5"
+    class="absolute m-3 top-0 right-0 bg-black border border-white/10 w-[300px] min-h-[50px] rounded-md p-5"
   >
-    <UForm :state="flowStore.node" class="space-y-4">
+    <UForm :state="flowStore.node.data" class="space-y-4">
       <UFormGroup label="Label">
-        <UInput v-model="flowStore.node.label" />
+        <UInput v-model="flowStore.node.data.label" />
+      </UFormGroup>
+      <UFormGroup label="Desc">
+        <UTextarea v-model="flowStore.node.data.desc" />
       </UFormGroup>
       <UFormGroup label="BackgroundColor">
-        <!-- <UInput v-model="{}" type="color" /> -->
+        <UInput v-model="flowStore.node.data.bgColor" type="color" />
       </UFormGroup>
     </UForm>
   </div>
