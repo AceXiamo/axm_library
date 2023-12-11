a73594785a73b378896cabd340fc5a69430b9918
AceXiamo <xiamo@qwq.link>
2023-12-11 19:00:27 +0800

    Refactor FlowNode.vue component

--- components/flow/FlowNode.vue
+++ components/flow/FlowNode.vue
@@ -2,11 +2,29 @@
   <Handle id="b" type="target" :position="Position.Left" />
   <div
     :class="[
-      `bg-gray-900 min-w-[100px] p-[10px_16px] text-center rounded-md box-border border border-solid`,
-      store.node?.id === $props.data.id ? 'border-emerald-500' : 'border-transparent',
+      `bg-gray-900 min-w-[300px] p-[10px_16px] rounded-md box-border border border-solid flex flex-col`,
+      store.node.id === $props.node.id
+        ? 'border-emerald-500'
+        : 'border-transparent',
     ]"
+    :style="{
+      backgroundColor: nodeData.bgColor,
+    }"
   >
-    <span class="text-white">{{ $props.data.label }}</span>
+    <div class="flex gap-[10px]">
+      <div class="flex flex-auto w-0 flex-col gap-[5px]">
+        <span class="text-white text-[14px]">{{ nodeData.label }}</span>
+        <span class="text-white/50 text-[12px]">{{ nodeData.desc }}</span>
+      </div>
+      <div class="w-max flex flex-col gap-[5px]">
+        <NodeButton icon="i-heroicons-pencil-square" class="text-purple-400" />
+        <NodeButton
+          icon="i-heroicons-squares-plus"
+          class="text-green-400"
+          @click="addChild"
+        />
+      </div>
+    </div>
   </div>
   <Handle id="a" type="source" :position="Position.Right" />
 </template>
@@ -14,8 +32,25 @@
 <script setup lang="ts">
 import type { NodeProps } from '@vue-flow/core'
 import { Position, Handle } from '@vue-flow/core'
-import { useFlowStore } from '~/store/flowStore';
+import { useFlowStore } from '~/store/flowStore'
+import type { FlowItem } from '.'
 
-const props = defineProps<{ data: NodeProps }>()
+const props = defineProps<{ node: NodeProps }>()
+const emits = defineEmits(['addNode'])
+const nodeData = props.node.data || {}
 const store = useFlowStore()
+
+const addChild = () => {
+  const id = Math.random().toString(36).substr(2, 5)
+  const node: FlowItem = {
+    id,
+    pid: nodeData.id,
+    label: `node ${id}`,
+    desc: '一段文字介绍/详情',
+    bgColor: '#101827',
+    children: [],
+  }
+
+  emits('addNode', node)
+}
 </script>
